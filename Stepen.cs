18977957ae7dbc82889ad8ff4529babe03d7cfb2
Olga Borisenko <olka_borisenko@mail.ru>
2022-12-16 20:04:14 +0300

    Возвели в степень

---
+++ Stepen.cs
@@ -0,0 +1,17 @@
+﻿// ЧИсло А возведение в степень B
+
+void NumberExtent(int number, int extent)
+{
+    double result = Math.Pow(number,extent);
+    Console.WriteLine($"{number} в степени {extent} = {result}");
+}
+int EnterNumber(string text)
+{
+    Console.Write(text);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+int number = EnterNumber("Введите число ");
+int extent = EnterNumber("Введите степень ");
+NumberExtent(number,extent);
+
